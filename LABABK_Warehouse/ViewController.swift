807b3cbbb1785bae5be534db14a8fa4f3beb3aff
Devang <nandani.trupti@gmail.com>
2017-06-08 14:52:28 +0530

    Initial Commit

---
+++ LABABK_Warehouse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LABABK_Warehouse
+//
+//  Created by iBlazing Mac Mini 3 on 08/06/17.
+//  Copyright © 2017 iBlazing Mac Mini 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
